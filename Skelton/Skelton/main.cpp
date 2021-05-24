e6ef51f7bc8cb1712394caebc61323f8dff8aeec
Takahide-Matsuo <2016030@s.asojuku.ac.jp>
2021-05-24 15:30:28 +0900

    スケルトンプログラム

--- Skelton/Skelton/main.cpp
+++ Skelton/Skelton/main.cpp
@@ -1,20 +1,26 @@
 #include <DxLib.h>
 #include<cassert>
 #include <string>
-
+#include<iomanip>
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+//int main()
 {
+	DxLib::SetGraphMode(640, 480, 32);
+	DxLib::ChangeWindowMode(true);
 	DxLib::SetWindowText(L"2016030_èºîˆëÈèG");
-	ChangeWindowMode(true);
-	DxLib_Init();
+
+	if (DxLib_Init())
+	{
+		return -1;
+	}
 
 	DxLib::SetDrawScreen(DX_SCREEN_BACK);
 	while (DxLib::ProcessMessage() == 0)
 	{
 		DxLib::ScreenFlip();
 	}
-	DxLib_End();
-	
-};
+	return 0;
+}
+
 
